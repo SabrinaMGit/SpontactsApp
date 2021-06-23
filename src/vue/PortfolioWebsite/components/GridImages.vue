92e0b25a4e8590a48d87bf4b692ff7abd8a3d6fb
Sabrina Meier <sabrina.meier@eecc.de>
2021-06-23 16:49:36 +0200

    changes

---
+++ src/vue/PortfolioWebsite/components/GridImages.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="(n, index) in images"
+      :key="index"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      <!--<router-link :to="`/gridPages/${n.name}`">-->
+        <v-img
+          :src="require(`@/assets/gridimages/${n.name}.png`)"
+          :lazy-src="require(`@/assets/gridimages/${n.name}.png`)"
+          aspect-ratio="1"
+          class="white lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+     <!-- </router-link>-->
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'GridImages',
+  props: ['images'],
+}
+</script>
--- src/vue/PortfolioWebsite/pages/mobile.vue
+++ src/vue/PortfolioWebsite/pages/mobile.vue
@@ -1,35 +1,15 @@
 <template>
-  <v-row>
-    <v-col
-      v-for="(n, index) in images"
-      :key="index"
-      class="d-flex child-flex"
-      cols="4"
-    >
-      <router-link :to="`/mobile/${n.name}`">
-        <v-img
-          :src="require(`@/assets/gridimages/${n.name}.png`)"
-          :lazy-src="require(`@/assets/gridimages/${n.name}.png`)"
-          aspect-ratio="1"
-          class="white lighten-2"
-        >
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-              ></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
-      </router-link>
-    </v-col>
-  </v-row>
+  <grid-images :images="images"></grid-images>
 </template>
 
 <script>
+import gridImages from '~/components/GridImages.vue';
+
 export default {
   name: 'mobile',
+  components: {
+    gridImages
+  },
   data: () => ({
     images: [
       { name: 'android' },
